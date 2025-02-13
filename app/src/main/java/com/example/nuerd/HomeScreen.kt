9f55059faca659578623a41fbb346557a530a129
koddaz <albert.niklasson@gmail.com>
2025-02-13 23:13:50 +0100

    Strukture set up

--- app/src/main/java/com/example/nuerd/HomeScreen.kt
+++ app/src/main/java/com/example/nuerd/HomeScreen.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +17,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -41,7 +38,7 @@ fun HomeScreen(
             ,
             horizontalArrangement = Arrangement.Center
             ) {
-            Text(text ="Nuerd", fontSize = 50.sp, color = Color.White, )
+            Text(text ="Nuerd", fontSize = 50.sp, color = Color.White )
         }
 
         Column(modifier
